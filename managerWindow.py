18da77a15df815328433ccdf763659e08bd5376c
Gabriel Biten <gabrielbiten.henrique@gmail.com>
2023-11-09 12:03:29 -0300

    Banco de dados remoto funcionando(PostgreSQL), começando gerenciador de usuários

--- managerWindow.py
+++ managerWindow.py
@@ -4,17 +4,52 @@ import psycopg2
 
 db_config = "dbname=automatizador user=postgres password=mpti3562 host=127.0.0.1"
 
-
-class ManagerWindow():
+class DatabaseManager:
     def __init__(self):
-        users = self.load_users()
+        self.conn = None
+
+    def connect(self):
+        try:
+            self.conn = psycopg2.connect(db_config)
+            return self.conn
+        except psycopg2.Error as e:
+            print("Erro ao conectar ao banco de dados: ", e)
+            return None
+
+    def close(self):
+        if self.conn:
+            self.conn.close()
+
+    def execute_query(self, query, data=None):
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute(query, data)
+                self.conn.commit()
+                return cursor
+            except psycopg2.Error as e:
+                print("Erro ao executar consulta no banco de dados: ", e)
+                return None
+
+    def delete_user(self, user_id):
+        if self.conn:
+            try:
+                cursor = self.conn.cursor()
+                cursor.execute("DELETE FROM users WHERE id = %s", (user_id,))
+                self.conn.commit()
+            except psycopg2.Error as e:
+                print("Erro ao excluir usuário: ", e)
+                return None
+
+
+class ManagerWindow:
+    def __init__(self):
+        self.db_manager = DatabaseManager()
         self.window = tk.Tk()
         self.window.geometry("600x500")
         self.window.title("Gerenciador de Usuários")
 
         self.users_listbox = tk.Listbox(self.window)
-        for user in users:
-            self.users_listbox.insert(tk.END, user[0] + " - " + user[1])
         self.users_listbox.pack()
 
         self.edit_button = tk.Button(self.window, text="Editar Usuário", command=self.edit_user)
@@ -23,18 +58,16 @@ class ManagerWindow():
         self.delete_button = tk.Button(self.window, text="Excluir Usuário", command=self.delete_user)
         self.delete_button.pack()
 
-    @staticmethod
-    def load_users():
-        try:
-            conn = psycopg2.connect(db_config)
-            cursor = conn.cursor()
-            cursor.execute("SELECT name, email FROM users")
+        self.load_users()
+
+    def load_users(self):
+        self.db_manager.connect()
+        cursor = self.db_manager.execute_query("SELECT id, name, email FROM users")
+        if cursor:
             users = cursor.fetchall()
-            print(users)
-            return users
-        except psycopg2.Error as error:
-            print("Erro ao carregar usuários: ", error)
-            return []
+            for user in users:
+                self.users_listbox.insert(tk.END, f"{user[0]} - {user[1]} - {user[2]}")
+        self.db_manager.close()
 
     def edit_user(self):
         selected_user = self.users_listbox.get(tk.ACTIVE)
@@ -51,6 +84,15 @@ class ManagerWindow():
         confirmed = tk.messagebox.askyesno("Confirmação", f"Tem certeza que deseja excluir o usuário: {selected_user}")
         if confirmed:
             print(f"Excluindo o usuário: {selected_user}")
+            user_id = int(selected_user.split(" - ")[0])
+            self.db_manager.connect()
+            if self.db_manager.delete_user(user_id):
+                tk.messagebox.showinfo("Sucesso", f"Usuário {user_id}, excluido com sucesso.")
+                self.load_users()
+                print("Usuário excluido com sucesso")
+            else:
+                tk.messagebox.showerror("Erro", "Erro ao excluir o usuário.")
+        self.db_manager.close()
 
 
 manage = ManagerWindow()
